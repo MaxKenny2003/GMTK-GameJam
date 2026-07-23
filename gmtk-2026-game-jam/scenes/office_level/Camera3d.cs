44a9903e13d17cae1e7e6a73a1f7332990810aed
realbraxeus <tedin2002@gmail.com>
2026-07-23 10:29:54 -0400

    Office scene camera fixed, still needs tuning

--- gmtk-2026-game-jam/scenes/office_level/Camera3d.cs
+++ gmtk-2026-game-jam/scenes/office_level/Camera3d.cs
@@ -1,21 +1,20 @@
 using Godot;
 using System;
 
-// This script is meant to move the camera from point A to point B, but it does not work and I'm eepy
+// Changed the tween to alter global position instead of just position and it worked - Zach
 public partial class Camera3d : Camera3D
 {
 	private Vector3 position;
 
 	[Export]
 	public Vector3 target_pos = new Vector3(0, 0, 0);
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
 	{
-        // GD.Print("Is current? ", Current);
-        GD.Print("Started move from ", Position, " to ", target_pos);
-        position = Position;
-        var tween = CreateTween();
-        tween.TweenProperty(this, "position", target_pos, 2.0f);
-    }
+		// GD.Print("Is current? ", Current);
+		GD.Print("Started move from ", Position, " to ", target_pos);
+		var tween = CreateTween();
+		tween.TweenProperty(this, "global_position", target_pos, 2.0f);
+	}
 
 }
