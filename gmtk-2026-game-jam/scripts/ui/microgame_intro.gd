2864f54b7b0f010a03c02203ed2fb8f2ba5f2008
RisingPhoenix730 <124209669+RisingPhoenix730@users.noreply.github.com>
2026-07-24 16:34:12 -0400

    Start Fly Swatter Game, Add logic for instructions UI

--- gmtk-2026-game-jam/scripts/ui/microgame_intro.gd
+++ gmtk-2026-game-jam/scripts/ui/microgame_intro.gd
@@ -1,14 +1,35 @@
 extends CanvasLayer
 @onready var timer: Timer = $Timer
 
+@onready var title_label: Label = $Control/GameTitleBackground/TitleLabel
+@onready var keyboard: TextureRect = $Control/HBoxContainer/Keyboard
+@onready var mouse: TextureRect = $Control/HBoxContainer/Mouse
+@onready var keyboards: Array[Texture2D] = [
+	preload("res://assets/2D/KeyboardTemplate.png"),
+	preload("res://assets/2D/KeyboardSpace.png"),
+	preload("res://assets/2D/KeyboardArrows.png"),
+	preload("res://assets/2D/KeyboardAll.png")
+]
+@onready var mouses: Array[Texture2D] = [
+	preload("res://assets/2D/MouseTemplate.png"),
+	preload("res://assets/2D/MouseLeft.png"),
+	preload("res://assets/2D/MouseMoveLeft.png")
+]
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-	
+
+func change_inputs(keyboard_index, mouse_index, game_title):
+	if (visible == false):
+		visible = true;
+		timer.start();
+	keyboard.texture = keyboards[keyboard_index];
+	mouse.texture = mouses[mouse_index];
+	title_label.text = game_title;
+
+
 func _on_timer_timeout() -> void:
 	visible = false;
