aff6b5c091f7827ddd736abaec05a8bd4179a45a
realbraxeus <tedin2002@gmail.com>
2026-07-26 01:27:44 -0400

    Revert "Merge branch 'main' into recovery"
    This reverts commit 2be5d50e6bc9a77b1a5a476cdb9d00b9cae19ed1, reversing
    changes made to 5219e7dadc36a64647553e87bbee1d67bcb56013.

--- gmtk-2026-game-jam/scripts/office_level/Asteroid.cs
+++ gmtk-2026-game-jam/scripts/office_level/Asteroid.cs
@@ -14,7 +14,7 @@ public partial class Asteroid : Node3D
     private WorldEnvironment worldEnvironment;
 
     [Export]
-    public float total_game_time = 270.0f;
+    public float total_game_time = 30.0f;
 
     public override void _Ready()
     {
@@ -30,6 +30,8 @@ public partial class Asteroid : Node3D
         var sky = env.Sky;
         var skyMaterial = (ProceduralSkyMaterial)sky.SkyMaterial;
 
+
+
         var tween = CreateTween();
         tween.TweenProperty(this, "global_position", asteroid_state_positions[asteroid_state], total_game_time)
             .SetTrans(Tween.TransitionType.Linear);
@@ -38,26 +40,22 @@ public partial class Asteroid : Node3D
         var tween3 = CreateTween();
         tween3.TweenProperty(env, "fog_light_color", new Color(119f / 255f, 7f / 255f, 21f / 255f), total_game_time);
 
-        flowController.Instance.set_move_camera(true);
         await ToSignal(GetTree().CreateTimer(total_game_time/5), "timeout");
         asteroid_state++;
         flowController.Instance.asteroid_state = asteroid_state;
         GD.Print("Asteroid state: ", asteroid_state);
 
-        flowController.Instance.set_move_camera(true);
         await ToSignal(GetTree().CreateTimer(total_game_time / 5), "timeout");
         asteroid_state++;
         flowController.Instance.asteroid_state = asteroid_state;
         GD.Print("Asteroid state: ", asteroid_state);
 
-        flowController.Instance.set_move_camera(true);
         await ToSignal(GetTree().CreateTimer(total_game_time / 5), "timeout");
         asteroid_state++;
         flowController.Instance.asteroid_state = asteroid_state;
         flowController.Instance.is_camera_shaking = true;
         GD.Print("Asteroid state: ", asteroid_state);
 
-        flowController.Instance.set_move_camera(true);
         await ToSignal(GetTree().CreateTimer(total_game_time / 5), "timeout");
         asteroid_state++;
         flowController.Instance.asteroid_state = asteroid_state;
