52d638135e58cf6dcd3c14343e4efed4a21814b5
realbraxeus <tedin2002@gmail.com>
2026-07-26 01:02:32 -0400

    sound effects

--- gmtk-2026-game-jam/scenes/office_level/Camera3d.cs
+++ gmtk-2026-game-jam/scenes/office_level/Camera3d.cs
@@ -11,18 +11,18 @@ public partial class Camera3d : Camera3D
 	private float time_to_stare = 1.0f;
 	private Vector3 position;
 
-    [Export]
-    public float shakeStrength = 0.01f;
+	[Export]
+	public float shakeStrength = 0.01f;
 
-    [Export]
+	[Export]
 	public Vector3 target_pos = new Vector3(0, 0, 0);
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		// GD.Print("Is current? ", Current);
-        position = Position;
-        GD.Print("Started move from ", Position, " to ", target_pos);
-        MoveCamera();
+		position = Position;
+		GD.Print("Started move from ", Position, " to ", target_pos);
+		MoveCamera();
 
 	}
 
@@ -34,30 +34,30 @@ public partial class Camera3d : Camera3D
 		flowController.Instance.set_looking_at_computer(true);
 		flowController.Instance.set_can_start_game(true);
 		flowController.Instance.set_can_move_camera(true);
-        flowController.Instance.set_move_camera(true);
+		flowController.Instance.set_move_camera(true);
 
-    }
+	}
 
 	public override void _Process(double delta)
 	{
 		if (flowController.Instance.move_Camera && flowController.Instance.can_move && !flowController.Instance.is_in_game)
 		{
 			flowController.Instance.set_can_start_game(false);
-            RotateCamera();
-            flowController.Instance.set_move_camera(false);
-        }
+			RotateCamera();
+			flowController.Instance.set_move_camera(false);
+		}
 
-        if(flowController.Instance.is_camera_shaking)
-        {
-            Vector3 offset = new Vector3
-                (
-                (float)(GD.Randf() * 2 - 1) * shakeStrength,
-                (float)(GD.Randf() * 2 - 1) * shakeStrength,
-                (float)(GD.Randf() * 2 - 1) * shakeStrength
-            );
-            Position = target_pos + offset;
-        }
-    }
+		if(flowController.Instance.is_camera_shaking)
+		{
+			Vector3 offset = new Vector3
+				(
+				(float)(GD.Randf() * 2 - 1) * shakeStrength,
+				(float)(GD.Randf() * 2 - 1) * shakeStrength,
+				(float)(GD.Randf() * 2 - 1) * shakeStrength
+			);
+			Position = target_pos + offset;
+		}
+	}
 
 	private async void RotateCamera()
 	{ 
@@ -79,7 +79,7 @@ public partial class Camera3d : Camera3D
 
 		await ToSignal(tween2, "finished");
 		flowController.Instance.set_looking_at_computer(true);
-        flowController.Instance.set_can_move_camera(true);
-        flowController.Instance.set_can_start_game(true);
+		flowController.Instance.set_can_move_camera(true);
+		flowController.Instance.set_can_start_game(true);
 	}
 }
